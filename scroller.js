fded7b834a71aa787019a84cc78a827aa5ba5dda
Peter Lind <petl@kea.dk>
2018-09-02 20:28:08 +0200

    Initial files - with pseudocode

---
+++ scroller.js
@@ -0,0 +1,34 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", init);
+
+
+
+function init() {
+    console.log("init");
+
+    // Get original text
+
+    // clear the original text from HTML
+
+    // start at position 0
+
+    // start the loop
+    setTimeout(loop, 100);
+}
+
+function loop() {
+    console.log("loop");
+    
+    // create the new text as a piece of the old one - 40 characters long
+
+    // display the new text
+
+    // move to the next character
+
+    // when we reach the end, start from the beginning
+    
+
+    // loop the loop
+    setTimeout(loop, 100);
+}
